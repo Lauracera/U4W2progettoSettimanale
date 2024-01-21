847373af7f3c4fdd3c52bd042bd5c74026f04dca
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2024-01-21 17:27:15 +0100

    Elemento

--- src/it/epicode/be/ElementoCatalogo.java
+++ src/it/epicode/be/ElementoCatalogo.java
@@ -2,4 +2,32 @@ package it.epicode.be;
 
 public class ElementoCatalogo implements Serializable{
     private static final long serialVersionUID = 1L;
+
+    private String codiceISBN;
+    private String titolo;
+    private int annoPubblicazione;
+    private int numeroPagine;
+
+    public ElementoCatalogo(String codiceISBN, String titolo, int annoPubblicazione, int numeroPagine) {
+        this.codiceISBN = codiceISBN;
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.numeroPagine = numeroPagine;
+    }
+
+    public String getCodiceISBN() {
+        return codiceISBN;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
 }
---
+++ src/it/epicode/be/Libro.java
@@ -0,0 +1,5 @@
+package it.epicode.be;
+
+public class Libro extends ElementoCatalogo{
+
+}
